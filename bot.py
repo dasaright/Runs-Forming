d16723b7c384e5315fb85d7c5b2a0a367ca659c9
dasaright <amasad1993@gmail.com>
2026-05-16 18:22:25 -0400

    Add files via upload

--- bot.py
+++ bot.py
@@ -198,7 +198,7 @@ def sort_and_split(signups):
 
 
 def build_embed(selected, waitlist, is_open):
-    embed = discord.Embed(title=":poggers: Guild Runs")
+    embed = discord.Embed(title="<:poggers:1505333070914392244> Guild Runs")
 
     hours, minutes = get_time_until_open()
 
